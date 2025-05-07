afeb123570dc8595aa87243c79fd514871befcfe
khanh-ptit <168727980+khanh-ptit@users.noreply.github.com>
2025-05-07 15:27:51 +0700

    add getSongBySinger api

--- controllers/client/singer.controller.js
+++ controllers/client/singer.controller.js
@@ -1,20 +1,57 @@
-const Singer = require("../../models/singer.model")
+const Singer = require("../../models/singer.model");
+const Song = require("../../models/song.model");
 
 module.exports.getSingerList = async (req, res) => {
   try {
     const singers = await Singer.find({
-      deleted: false
+      deleted: false,
     }).sort({
-      position: "desc"
-    })
-    console.log(singers)
+      position: "desc",
+    });
+    console.log(singers);
     res.json({
-      singers
-    })
+      singers,
+    });
   } catch (error) {
     res.status(500).json({
       code: 500,
-      message: "Đã xảy ra lỗi khi lấy danh sách ca sĩ!"
-    })
+      message: "Đã xảy ra lỗi khi lấy danh sách ca sĩ!",
+    });
   }
-}
+};
+
+module.exports.getSongBySinger = async (req, res) => {
+  try {
+    const singerId = req.params.singerId;
+    const singerInfo = await Singer.findOne({ _id: singerId }).select(
+      "-updatedBy"
+    );
+    const songs = await Song.find({
+      singerId: singerId,
+      status: "active",
+      deleted: false,
+    })
+      .lean()
+      .sort({
+        position: "asc",
+      })
+      .select("-lyrics");
+    for (const item of songs) {
+      const singerId = item.singerId;
+      const singerInfo = await Singer.findOne({ _id: singerId });
+      if (singerInfo) {
+        item.singerName = singerInfo.fullName;
+      }
+    }
+    res.json({
+      singerInfo,
+      songs,
+    });
+  } catch {
+    console.log(error);
+    res.status(500).json({
+      code: 500,
+      message: "Đã xảy ra lỗi khi lấy danh sách bài hát theo chủ đề!",
+    });
+  }
+};
--- routes/client/singer.route.js
+++ routes/client/singer.route.js
@@ -5,4 +5,6 @@ const controller = require("../../controllers/client/singer.controller");
 
 router.get("/", controller.getSingerList);
 
+router.get("/:singerId", controller.getSongBySinger);
+
 module.exports = router;
