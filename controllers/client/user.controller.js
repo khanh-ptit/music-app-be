f87a1bd89acce220dc3b72e4f393a4ffdaa2886a
khanh-ptit <168727980+khanh-ptit@users.noreply.github.com>
2025-05-12 23:03:16 +0700

    Update user.controller.js

--- controllers/client/user.controller.js
+++ controllers/client/user.controller.js
@@ -1,6 +1,7 @@
 const User = require("../../models/user.model");
 const Song = require("../../models/song.model");
 const FavoriteSong = require("../../models/favorite-song.model");
+const Singer = require("../../models/singer.model");
 const md5 = require("md5");
 
 module.exports.login = async (req, res) => {
@@ -77,7 +78,7 @@ module.exports.getUserProfile = async (req, res) => {
 
 module.exports.getFavoriteSong = async (req, res) => {
   try {
-    const tokenUser = req.headers["authorization"];
+    const tokenUser = req.headers["authorization"].split(" ")[1];
 
     if (!tokenUser) {
       return res.status(401).json({
@@ -103,17 +104,26 @@ module.exports.getFavoriteSong = async (req, res) => {
     // Fetching favorite songs
     const songs = await FavoriteSong.find({
       userId: userId,
+    }).lean();
+
+    const songIds = songs.map((song) => song.songId);
+    // console.log("Song IDs: ", songIds);
+
+    const songInfoPromises = songIds.map((songId) => {
+      return Song.findById(songId); // Assuming Song is a model for songs in your DB
     });
 
-    const songIds = songs.map(song => song.songId);
-    console.log("Song IDs: ", songIds);
+    const songInfos = await Promise.all(
+      songIds.map(async (songId) => {
+        const song = await Song.findById(songId).lean();
+        if (!song) return null;
 
-    const songInfoPromises = songIds.map(songId => {
-      return Song.findById(songId);  // Assuming Song is a model for songs in your DB
-    });
+        const singer = await Singer.findById(song.singerId).lean();
+        song.singerName = singer ? singer.fullName : "Unknown";
 
-    const songInfos = await Promise.all(songInfoPromises);
-    console.log("Song Infos: ", songInfos);
+        return song;
+      })
+    );
 
     return res.status(200).json({
       code: 200,
@@ -121,7 +131,7 @@ module.exports.getFavoriteSong = async (req, res) => {
       email: user.email,
       phone: user.phone,
       avatar: user.avatar,
-      favoriteSongs: songInfos,  // Sending the song details in response
+      favoriteSongs: songInfos, // Sending the song details in response
     });
   } catch (error) {
     console.error(error);
